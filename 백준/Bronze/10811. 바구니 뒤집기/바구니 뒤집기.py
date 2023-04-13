5ddaa8092a6eb2402c43d8f1fd38ed1a5a31e527
조찬익 <112329190+Bandozen@users.noreply.github.com>
2023-04-13 16:29:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+basket = [i for i in range(1, n+1)]
+# print(basket)
+for _ in range(m):
+    i, j = map(int, input().split())
+    basket[i-1:j]= reversed(basket[i-1:j])
+print(*basket)
