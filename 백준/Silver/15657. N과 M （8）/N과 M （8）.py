a3a7d4a423e1f8024d81ddc9d35c66d97683c2ff
조찬익 <112329190+Bandozen@users.noreply.github.com>
2023-04-16 22:47:57 +0900

    [Silver III] Title: N과 M (8), Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+tmp = []
+
+def dfs(start):
+    if len(tmp) == m:
+        print(*tmp)
+        return
+    for i in range(start, n):
+            tmp.append(nums[i])
+            dfs(i)
+            tmp.pop()
+
+dfs(0)
