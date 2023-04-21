a9238659ca9451a937fd4ed5cc5fbb94103976c1
조찬익 <112329190+Bandozen@users.noreply.github.com>
2023-04-21 17:30:27 +0900

    [level 1] Title: 시저 암호, Time: 2.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    new_s = list(s)
+    
+    for i in range(len(new_s)):
+        if new_s[i].isupper():
+            new_s[i] = chr((ord(new_s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif new_s[i].islower():
+            new_s[i] = chr((ord(new_s[i]) - ord('a') + n) % 26 + ord('a'))
+            
+    return ''.join(new_s)
